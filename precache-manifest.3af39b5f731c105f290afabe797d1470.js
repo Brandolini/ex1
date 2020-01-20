8bd024cec4d4ffc783d895d6a62baade70a1d1a2
Brandolini <revitoop@gmail.com>
2020-01-20 02:57:09 -0600

    Add files via upload

---
+++ precache-manifest.3af39b5f731c105f290afabe797d1470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed3cf5002be563a0d83daabf6c06b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5c3b46b76feb3028b54",
+    "url": "/static/css/main.5ec45416.chunk.css"
+  },
+  {
+    "revision": "3eb8a1a52041ba9063b0",
+    "url": "/static/js/2.43d17c82.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.43d17c82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5c3b46b76feb3028b54",
+    "url": "/static/js/main.506362c5.chunk.js"
+  },
+  {
+    "revision": "5bad826cc55e7f595e8e",
+    "url": "/static/js/runtime-main.41c7cbc0.js"
+  }
+]);
